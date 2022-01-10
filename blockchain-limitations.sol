a8886f198c1fd72c9dd302dbfe22e3d4cb2f9bf6
Njoguu <njoguuallan@gmail.com>
2022-01-10 20:53:08 +0300

    using structs

--- blockchain-limitations.sol
+++ blockchain-limitations.sol
@@ -1,47 +1,65 @@
 pragma solidity 0.8.10;
 
+
+// structs
+struct Person{
+	uint age;
+	string name;
+	uint balance;
+}
+
 contract MyContract{
+
+    mapping(address => Person) people;
+
+    function createNewPerson(uint age, string memory name) public{
+        Person memory person = Person(age, name, 0);
+        people[msg.sender] = person;
+    }
+}
+
+//contract MyContract{
 	
 	//	1. ARRAYS
 	// creating an array
-	uint[] public arr;
+//	uint[] public arr;
 
 	// function to modify the array
-	function modifyArray(uint value) public{
-		arr.push(value);
-	}
+//	function modifyArray(uint value) public{
+//		arr.push(value);
+//	}
 
 	// function to get the length of the array
-	function getLength() external view returns (uint){
-		arr.length;
-	}
+//	function getLength() external view returns (uint){
+//		arr.length;
+//	}
 
 	//	2. STRINGS
 	// creating a string
-	string public sayhello  = "hello world";
+//	string public sayhello  = "hello world";
 
 	// function to set string
-	function setString(string memory value) public{
-		sayhello = value;
-	}
+//	function setString(string memory value) public{
+//		sayhello = value;
+//	}
 
 	//	3. LOOPS
 	// starting a loop
-	function loop(uint n) public pure{
-		uint sum = 0;
+//	function loop(uint n) public pure{
+//		uint sum = 0;
 		// for loop
-		for (uint x; x < n; x++){
-			sum += x;
-		}
+//		for (uint x; x < n; x++){
+//			sum += x;
+//		}
 
-		// while loop
-		uint x = 0;
-		while (x < n) {
-			sum += x;
-			x++;
+	// while loop
+//		uint x = 0;
+//		while (x < n) {
+//			sum += x;
+//			x++;
 
-		}
+//		}
 
-	}
+//	}
 
-}
+// }
